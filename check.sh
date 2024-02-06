51f54ee81d7efea2f4c4c1f367797c53702cce05
Luiz Paulo S Zanetti <97256354+lpzanetti@users.noreply.github.com>
2024-02-06 16:32:20 -0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,5 +2,4 @@
 
 # This is a simple shell script to echo "TESTING"
 echo "TESTING"
-killall -9 python3
-nohup /usr/bin/python3 /var/www/sistemas/auxiliar/imprime/check.py > /output.log 2>&1 &
+nohup /usr/bin/python3 /check.py > /output.log 2>&1 &
